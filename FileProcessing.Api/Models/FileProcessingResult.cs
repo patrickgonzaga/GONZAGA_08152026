0afeea03d1e1393e02ac218c8483a307dac5a64d
patgonzaga <p.gonzaga.perosonal@gmail.com>
2026-08-15 19:41:43 +0800

    feat(CH-2): add CSV Processing Models

---
+++ FileProcessing.Api/Models/FileProcessingResult.cs
@@ -0,0 +1,9 @@
+﻿namespace FileProcessing.Api.Models
+{
+    public class FileProcessingResult
+    {
+        public string FileName { get; init; } = string.Empty;
+        public int RecordCount { get; init; }
+        public decimal AverageAmount { get; init; }
+    }
+}
---
+++ FileProcessing.Api/Models/SalesRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace FileProcessing.Api.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+    }
+}
