bff792346dbcc22bca7b0738e08b5c2f96abff34
patgonzaga <p.gonzaga.perosonal@gmail.com>
2026-08-16 17:33:21 +0800

    feat(CH-3): add FileProcessing Models

---
+++ FileProcessing.Api/Models/FileProcessingLogEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace FileProcessing.Api.Models
+{
+    public class FileProcessingLogEntry
+    {
+        public string FileName { get; init; } = string.Empty;
+        public int RecordCount { get; init; }
+        public decimal AverageAmount { get; init; }
+        public DateTimeOffset ProcessedAt { get; init; }
+        public long Duration { get; init; }
+    }
+}
