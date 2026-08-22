3a0a918b1916dced8ccf9134f443e1e94431fc53
patgonzaga <p.gonzaga.perosonal@gmail.com>
2026-08-22 22:15:24 +0800

    Refactor: Update Solution Naming and Move to corresponding folders.

--- FileProcessing.Api/Models/FileProcessingResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileProcessing.Api.Models
-{
-    public class FileProcessingResult
-    {
-        public string FileName { get; init; } = string.Empty;
-        public int RecordCount { get; init; }
-        public decimal AverageAmount { get; init; }
-    }
-}
--- FileProcessing.Api/Models/SalesRecord.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileProcessing.Api.Models
-{
-    public class SalesRecord
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public decimal Amount { get; set; }
-    }
-}
